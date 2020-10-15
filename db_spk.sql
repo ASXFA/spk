823a57e7110ac0361fe1bb85260615392c4cb0e4
ASXFA <fachri.agustian12@gmail.com>
2020-10-15 14:26:33 +0700

    progres

--- db_spk.sql
+++ db_spk.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:3306
--- Waktu pembuatan: 12 Okt 2020 pada 11.15
+-- Waktu pembuatan: 15 Okt 2020 pada 07.25
 -- Versi server: 5.7.24
 -- Versi PHP: 7.2.19
 
@@ -69,11 +69,32 @@ INSERT INTO `kriteria` (`id`, `nama_kriteria`) VALUES
 
 CREATE TABLE `kriteria_nilai` (
   `id` int(3) NOT NULL,
-  `beasiswa_id` int(3) NOT NULL,
   `kriteria_id_from` int(3) NOT NULL,
   `kriteria_id_to` int(3) NOT NULL,
-  `nilai` int(1) NOT NULL
+  `nilai` double NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data untuk tabel `kriteria_nilai`
+--
+
+INSERT INTO `kriteria_nilai` (`id`, `kriteria_id_from`, `kriteria_id_to`, `nilai`) VALUES
+(681, 3, 4, 0.33),
+(682, 3, 5, 0.2),
+(683, 3, 7, 0.14),
+(684, 4, 4, 1),
+(685, 4, 3, 3),
+(686, 4, 5, 0.33),
+(687, 4, 7, 0.2),
+(688, 5, 3, 5),
+(689, 5, 5, 1),
+(690, 5, 7, 0.33),
+(691, 7, 3, 7),
+(692, 7, 4, 5),
+(693, 7, 7, 1),
+(694, 5, 4, 3),
+(695, 7, 5, 3),
+(696, 3, 3, 1);
 
 -- --------------------------------------------------------
 
@@ -83,10 +104,19 @@ CREATE TABLE `kriteria_nilai` (
 
 CREATE TABLE `kriteria_prioritas` (
   `id` int(3) NOT NULL,
-  `beasiswa_id` int(3) NOT NULL,
   `kriteria_id` int(3) NOT NULL,
   `prioritas` float NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data untuk tabel `kriteria_prioritas`
+--
+
+INSERT INTO `kriteria_prioritas` (`id`, `kriteria_id`, `prioritas`) VALUES
+(109, 4, 0.26),
+(110, 3, 0.56),
+(111, 7, 0.06),
+(112, 5, 0.12);
 
 -- --------------------------------------------------------
 
@@ -119,8 +149,30 @@ CREATE TABLE `nilai_pasang` (
   `id` int(3) NOT NULL,
   `nilai_id_from` int(3) NOT NULL,
   `nilai_id_to` int(3) NOT NULL,
-  `nilai_pasangan` int(1) NOT NULL
+  `nilai_pasangan` double NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data untuk tabel `nilai_pasang`
+--
+
+INSERT INTO `nilai_pasang` (`id`, `nilai_id_from`, `nilai_id_to`, `nilai_pasangan`) VALUES
+(33, 1, 2, 0.33),
+(34, 1, 3, 0.2),
+(35, 1, 4, 0.14),
+(36, 2, 2, 1),
+(37, 2, 3, 0.33),
+(38, 2, 4, 0.2),
+(39, 2, 1, 3),
+(40, 1, 1, 1),
+(41, 3, 4, 0.33),
+(42, 3, 3, 1),
+(43, 4, 1, 7),
+(44, 4, 2, 5),
+(45, 4, 3, 3),
+(46, 3, 1, 5),
+(47, 3, 2, 3),
+(48, 4, 4, 1);
 
 -- --------------------------------------------------------
 
@@ -131,10 +183,19 @@ CREATE TABLE `nilai_pasang` (
 CREATE TABLE `nilai_prioritas` (
   `id` int(3) NOT NULL,
   `nilai_id` int(3) NOT NULL,
-  `nilai_pasangan_id` int(1) NOT NULL,
-  `prioritas` float NOT NULL,
-  `nilai_sub_kriteria` float NOT NULL
+  `prioritas` double NOT NULL,
+  `subprioritas` double NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data untuk tabel `nilai_prioritas`
+--
+
+INSERT INTO `nilai_prioritas` (`id`, `nilai_id`, `prioritas`, `subprioritas`) VALUES
+(13, 4, 0.06, 0.11),
+(14, 3, 0.12, 0.21),
+(15, 2, 0.26, 0.46),
+(16, 1, 0.56, 1);
 
 -- --------------------------------------------------------
 
@@ -172,11 +233,6 @@ CREATE TABLE `subkriteria` (
   `id` int(3) NOT NULL,
   `kriteria_id` int(3) NOT NULL,
   `nama_subkriteria` varchar(100) NOT NULL,
-  `tipe` varchar(5) NOT NULL,
-  `nilai_minumum` float DEFAULT NULL,
-  `nilai_maksimum` float DEFAULT NULL,
-  `op_min` varchar(2) DEFAULT NULL,
-  `op_max` varchar(2) DEFAULT NULL,
   `nilai_id` int(1) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
@@ -184,8 +240,23 @@ CREATE TABLE `subkriteria` (
 -- Dumping data untuk tabel `subkriteria`
 --
 
-INSERT INTO `subkriteria` (`id`, `kriteria_id`, `nama_subkriteria`, `tipe`, `nilai_minumum`, `nilai_maksimum`, `op_min`, `op_max`, `nilai_id`) VALUES
-(1, 1, '4.00', 'text', NULL, NULL, NULL, NULL, 1);
+INSERT INTO `subkriteria` (`id`, `kriteria_id`, `nama_subkriteria`, `nilai_id`) VALUES
+(1, 3, '4.00', 1),
+(3, 3, '3.60 - 3.99', 0),
+(4, 3, '3.00 - 3.59', 0),
+(5, 3, '> 2.99', 0),
+(6, 4, '<= 1.000.000', 0),
+(7, 4, '1.100.000 - 2.000.000', 0),
+(8, 4, '2.100.000 - 3.000.000', 0),
+(9, 4, '> 3.000.000', 0),
+(10, 5, 'Nasional', 0),
+(11, 5, 'Provinsi', 0),
+(12, 5, 'Kota / Kabupaten', 0),
+(13, 5, 'Kampus', 0),
+(14, 7, 'Nasional', 0),
+(15, 7, 'Provinsi', 0),
+(16, 7, 'Kota / Kabupaten', 0),
+(17, 7, 'Kampus', 0);
 
 -- --------------------------------------------------------
 
@@ -329,31 +400,31 @@ ALTER TABLE `kriteria`
 -- AUTO_INCREMENT untuk tabel `kriteria_nilai`
 --
 ALTER TABLE `kriteria_nilai`
-  MODIFY `id` int(3) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(3) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=697;
 
 --
 -- AUTO_INCREMENT untuk tabel `kriteria_prioritas`
 --
 ALTER TABLE `kriteria_prioritas`
-  MODIFY `id` int(3) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(3) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=113;
 
 --
 -- AUTO_INCREMENT untuk tabel `nilai_kategori`
 --
 ALTER TABLE `nilai_kategori`
-  MODIFY `id` int(3) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id` int(3) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- AUTO_INCREMENT untuk tabel `nilai_pasang`
 --
 ALTER TABLE `nilai_pasang`
-  MODIFY `id` int(3) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(3) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=49;
 
 --
 -- AUTO_INCREMENT untuk tabel `nilai_prioritas`
 --
 ALTER TABLE `nilai_prioritas`
-  MODIFY `id` int(3) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(3) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
 
 --
 -- AUTO_INCREMENT untuk tabel `persyaratan`
@@ -371,7 +442,7 @@ ALTER TABLE `peserta`
 -- AUTO_INCREMENT untuk tabel `subkriteria`
 --
 ALTER TABLE `subkriteria`
-  MODIFY `id` int(3) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int(3) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
 
 --
 -- AUTO_INCREMENT untuk tabel `user`
